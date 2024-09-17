e0b976bd1a773f34f231c21adbed2b5084acc602
BIBA52 <varya_pankova@inbox.ru>
2024-09-17 18:00:32 +1000

    Add files via upload

---
+++ лол/javascript/script.js
@@ -0,0 +1,60 @@
+// let age = prompt ('возраст?');
+// let message = (age<3)? 'Здравствуй малыш!':
+// (age<18)? 'Привет!':
+// (age<100)? 'Здравствуйте!':
+// alert(message)
+// let accessAllowed = (age>18)? true: false;
+
+
+// let name = ['rt', false, 56]
+// console.log (name[0]);
+// name.push('teo', 356);
+// name[1]=3456789;
+// console.log(name);
+// console.log(name.length);
+
+
+// let userName = 'Вася';
+//     function showMessage(){
+//         let message = 'привет, ' + userName;
+//         alert(message);
+//             }
+//             showMessage();
+
+
+// function great(){
+//         alert('Всем привет');
+//     }
+//     great();
+//     great();
+
+
+// function great(from, text){
+//     alert(from+':'+text);
+// }
+// great('Аня'+'Привет');
+// great('Аня', 'Как дела?');
+
+
+// function checkAge(age) {
+// if (age>= 18) {
+//     return true 
+// }else{ 
+//  return confirm('A родители разрешили?') 
+// }
+// }
+
+
+//     let age = prompt('Сколько вам лет?', 18)
+//         if (checkAge(age)){
+//  alert('Доступ получен') 
+//     } else {
+// alert('Доступ закрыт')
+//     }
+
+
+// let age = prompt("Сколько вам лет?, 18");
+// let welcome = (age<18)?
+// () => alert('Привет!'):
+// () => alert ("Здравствуйте!");
+// welcome();
